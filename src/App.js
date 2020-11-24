201353ce0c21bea9e800d4e6c9f6a43a6c904447
nareshyeddu <nareshyeddu@gmail.com>
2020-11-24 11:49:11 +0530

    Event Bind and props

--- src/App.js
+++ src/App.js
@@ -6,15 +6,25 @@ import NameList from './components/NameList'
 import PostList from './components/PostList'
 import PostForm from './components/PostForm'
 import Form from './components/Form'
+import EventBind from './components/EventBind'
+import Greet from './components/Greet'
 
 function App() {
   return (
     <div className="App">
+      {/* <Greet name="Bruce" heroName="Batman">
+          <p>This is children props</p>
+        </Greet>
+        <Greet name="Clark" heroName="Superman">
+          <button>Action</button>
+        </Greet>
+        <Greet name="Diana" heroName="Wonder Woman" /> */}
       {/* <Message/> */}
       {/* <Counter/> */}
       {/* <NameList/> */}
       {/* <PostList/> */}
       <PostForm/>
+      <EventBind/>
       {/* <Form/> */}
     </div>
   );
